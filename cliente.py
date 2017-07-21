b7a372d10b2e0c1aab9b45af6a42b493dddb86ef
pmario2 <587222@comp.ufscar.br>
2017-07-21 02:15:49 -0300

    Delete cliente.py

--- cliente.py
+++
@@ -1,8 +0,0 @@
-import socket
-HOST = '127.0.0.1'
-PORT = 8001 
-tcp =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp.connect((HOST,PORT))
-msg = raw_input("digite algo: ")
-tcp.send(msg)
-tcp.close()
